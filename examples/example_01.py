c92ad4127bde64af89731144982ab94e3726bfde
Aslan Noorghasemi <anoorghasemi@gmail.com>
2024-11-17 10:06:07 -0500

    updated examples and readme

--- examples/example_01.py
+++ examples/example_01.py
@@ -1,13 +1,57 @@
 import keepdelta as kd
 
 
+# profile of a person
+old = {
+    "info": {
+        "name": "Alice",
+        "age": 30,
+        "is_student": True,
+        "grades": [85.5, 90.0, 78],
+        "preferences": (
+            "chocolate", 
+            {"sports": {"football", "tennis"}},
+        ),
+    },
+    "meta": {
+        "attributes": [
+            {"id": 1, "value": "active"},
+            {"id": 2, "value": "inactive"}
+        ],
+        "settings": {
+            "dark_mode": True,
+            "font_size": 14,
+            "scale": 1.25
+        }
+    }
+}
 
-old = (1, 2, 3)
-new = (2, 3, 4)
-#print(set(list(new)))
+# profile of that person one year later
+new = {
+    "info": {
+        "name": "Alice",
+        "age": 31,
+        "is_student": False,
+        "grades": [85.5, 90.0, 78],
+        "preferences": (
+            "coffee", 
+            {"sports": {"football", "bodybuilding"}},
+        ),
+    },
+    "meta": {
+        "attributes": [
+            {"id": 1, "value": "inactive"},
+            {"id": 2, "value": "inactive"}
+        ],
+        "settings": {
+            "dark_mode": True,
+            "font_size": 14,
+            "scale": 1.25
+        }
+    }
+}
+
 delta = kd.create(old, new)
 print(delta)
 var = kd.apply(old, delta)
-print(var)
-#print(set([{0: 1, 1: 1, 2: 1}]))
-#print('a' != 0)
+print(var == new)
