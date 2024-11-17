c7901645e9e03bfe92dbe70676ea2f5835f6e37f
Aslan Noorghasemi <anoorghasemi@gmail.com>
2024-11-17 10:33:12 -0500

    updated readme

--- examples/example_02.py
+++ examples/example_02.py
@@ -1,5 +1,31 @@
 import keepdelta as kd
 
+# Initial data
+old = {
+    "info": {
+        "name": "Alice",
+        "age": 30,
+        "is_student": True,
+        "preferences": (
+            "chocolate", 
+            {"sports": {"football", "tennis"}},
+        ),
+    }
+}
 
-old = (1, 2, 3)
-new = (2, 3, 4)
+# Updated data
+new = {
+    "info": {
+        "name": "Alice",
+        "age": 31,
+        "is_student": False,
+        "preferences": (
+            "coffee", 
+            {"sports": {"football", "bodybuilding"}},
+        ),
+    }
+}
+
+# Create delta
+delta = kd.create(old, new)
+print(delta)
