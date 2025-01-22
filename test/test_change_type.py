d89add9ca4c61d0a78fdf7ef32ca5e83799b54ee
aslan-ng <anoorghasemi@gmail.com>
2025-01-22 17:35:03 -0500

    unit tests for variable type change is completed.

--- test/test_change_type.py
+++ test/test_change_type.py
@@ -5,10 +5,10 @@ import keepdelta as kd
 
 class TestChangeTypeBool(unittest.TestCase):
 
-    def test_bool_to_int(self):
+    def test_bool_to_complex(self):
         old = True
-        new = 10
-        delta = 10
+        new = 1 + 1j
+        delta = 1 + 1j
         self.assertEqual(kd.create(old, new), delta)
         self.assertEqual(kd.apply(old, delta), new)
 
@@ -19,14 +19,14 @@ class TestChangeTypeBool(unittest.TestCase):
         self.assertEqual(kd.create(old, new), delta)
         self.assertEqual(kd.apply(old, delta), new)
 
-    def test_bool_to_complex(self):
+    def test_bool_to_int(self):
         old = True
-        new = 10 + 10j
-        delta = 10 + 10j
+        new = 10
+        delta = 10
         self.assertEqual(kd.create(old, new), delta)
         self.assertEqual(kd.apply(old, delta), new)
 
-    def test_bool_to_string(self):
+    def test_bool_to_str(self):
         old = True
         new = 'hello'
         delta = 'hello'
@@ -35,15 +35,63 @@ class TestChangeTypeBool(unittest.TestCase):
 
 
 class TestChangeTypeComplex(unittest.TestCase):
-    pass
+
+    def test_complex_to_bool(self):
+        old = 1 + 1j
+        new = True
+        delta = True
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_complex_to_float(self):
+        old = 1 + 1j
+        new = 1.1
+        delta = 1.1
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_complex_to_int(self):
+        old = 1 + 1j
+        new = 1
+        delta = 1
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_complex_to_str(self):
+        old = 1 + 1j
+        new = 'hello'
+        delta = 'hello'
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
 
 
 class TestChangeTypeFloat(unittest.TestCase):
 
+    def test_float_to_bool(self):
+        old = 1.1
+        new = True
+        delta = True
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_float_to_complex(self):
+        old = 1.1
+        new = 1 + 1j
+        delta = 1 + 1j
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
     def test_float_to_int(self):
-        old = 10.1
-        new = 11
-        delta = 11
+        old = 1.1
+        new = 1
+        delta = 1
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_float_to_str(self):
+        old = 1.1
+        new = 'hello'
+        delta = 'hello'
         self.assertEqual(kd.create(old, new), delta)
         self.assertEqual(kd.apply(old, delta), new)
 
@@ -51,22 +99,63 @@ class TestChangeTypeFloat(unittest.TestCase):
 class TestChangeTypeInt(unittest.TestCase):
 
     def test_int_to_bool(self):
-        old = 10
+        old = 1
         new = True
         delta = True
         self.assertEqual(kd.create(old, new), delta)
         self.assertEqual(kd.apply(old, delta), new)
 
+    def test_int_to_complex(self):
+        old = 1
+        new = 1 + 1j
+        delta = 1 + 1j
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
     def test_int_to_float(self):
-        old = 10
-        new = 11.1
-        delta = 11.1
+        old = 1
+        new = 1.1
+        delta = 1.1
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_int_to_str(self):
+        old = 1
+        new = 'hello'
+        delta = 'hello'
         self.assertEqual(kd.create(old, new), delta)
         self.assertEqual(kd.apply(old, delta), new)
 
 
 class TestChangeTypeStr(unittest.TestCase):
-    pass
+    
+    def test_str_to_bool(self):
+        old = 'hello'
+        new = True
+        delta = True
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_str_to_complex(self):
+        old = 'hello'
+        new = 1 + 1j
+        delta = 1 + 1j
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_str_to_float(self):
+        old = 'hello'
+        new = 1.1
+        delta = 1.1
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
+
+    def test_str_to_int(self):
+        old = 'hello'
+        new = 1
+        delta = 1
+        self.assertEqual(kd.create(old, new), delta)
+        self.assertEqual(kd.apply(old, delta), new)
 
 
 if __name__ == '__main__':
