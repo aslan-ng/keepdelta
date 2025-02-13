7963020c43715b3bf3634c7ad349a173c2ab3343
aslan-ng <anoorghasemi@gmail.com>
2025-02-12 19:41:23 -0500

    making internal commands more human-readable.

--- keepdelta/config.py
+++ keepdelta/config.py
@@ -5,6 +5,6 @@ Commands (keys) reserved for internal data handling
 keys = {
     'nothing': 'N07H1NG',  # No changes are required
     'delete': 'D3L373', # Key to be removed from dict
-    'add to set': 'a',  # Elements to be added to set
-    'remove from set': 'r',  # Elements to be removed from set
+    'add to set': 'add',  # Elements to be added to set
+    'remove from set': 'remove',  # Elements to be removed from set
 }
