87b40936e07f3693ebe7849f37445bc1a1428b62
Aslan Noorghasemi <anoorghasemi@gmail.com>
2025-03-14 13:27:20 -0400

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="keepdelta",
-    version="0.1.0",
+    version="0.1.1",
     description="Efficient Delta Management for Python Data Structures",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
