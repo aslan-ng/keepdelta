83ee8762e196cedd10c8a7c2293c9944daffe653
Chris McComb <ccmcc2012@gmail.com>
2025-03-25 09:58:18 -0400

    Review (#2)
    * Enhances comments
    
    * Small updates to documentation
    
    * Edits to paper

---
+++ tests/test_collections/__init__.py
@@ -0,0 +1,4 @@
+"""
+KeepDelta supports the creation of deltas when the inputs are collections.
+This set of tests targets that feature.
+"""
---
+++ tests/test_primitives/__init__.py
@@ -0,0 +1,4 @@
+"""
+KeepDelta supports the creation of deltas over standard primitives.
+This set of tests targets that feature.
+"""
